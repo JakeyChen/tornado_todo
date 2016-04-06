ba59b386bc48c83681841b4cfb19331762f13410
Jakey.Chen <281743668@qq.com>
2016-04-06 17:35:25 +0800

    add knoctout.js

--- static/js/todo.js
+++ static/js/todo.js
@@ -20,25 +20,60 @@ function newTodo() {
             }
         });
     }
-    // if (emptyCheck("todo_text")){
-    //     $.ajax({
-    //         url: "http://www.hzhuti.com",    //请求的url地址
-    //         dataType: "json",   //返回格式为json
-    //         async: true, //请求是否异步，默认为异步，这也是ajax重要特性
-    //         data: { "id": "value" },    //参数值
-    //         type: "GET",   //请求方式
-    //         beforeSend: function() {
-    //             //请求前的处理
-    //         },
-    //         success: function(req) {
-    //             //请求成功时处理
-    //         },
-    //         complete: function() {
-    //             //请求完成的处理
-    //         },
-    //         error: function() {
-    //             //请求出错处理
-    //         }
-    //     });
-    // }
 }
+
+var PlanetsModel = function() {
+    this.planets = ko.observableArray([
+        { name: "Mercury", type: "rock"},
+        { name: "Venus", type: "rock"},
+        { name: "Earth", type: "rock"},
+        { name: "Mars", type: "rock"},
+        { name: "Jupiter", type: "gasgiant"},
+        { name: "Saturn", type: "gasgiant"},
+        { name: "Uranus", type: "gasgiant"},
+        { name: "Neptune", type: "gasgiant"}
+    ]);
+ 
+    this.typeToShow = ko.observable("all");
+    this.displayAdvancedOptions = ko.observable(false);
+ 
+    this.addPlanet = function(type) {
+        this.planets.push({
+            name: "New planet",
+            type: type
+        });
+    };
+ 
+    this.planetsToShow = ko.pureComputed(function() {
+        // Represents a filtered list of planets
+        // i.e., only those matching the "typeToShow" condition
+        var desiredType = this.typeToShow();
+        if (desiredType == "all") return this.planets();
+        return ko.utils.arrayFilter(this.planets(), function(planet) {
+            return planet.type == desiredType;
+        });
+    }, this);
+ 
+    // Animation callbacks for the planets list
+    this.showPlanetElement = function(elem) { if (elem.nodeType === 1) $(elem).hide().slideDown() }
+    this.hidePlanetElement = function(elem) { if (elem.nodeType === 1) $(elem).slideUp(function() { $(elem).remove(); }) }
+};
+ 
+// Here's a custom Knockout binding that makes elements shown/hidden via jQuery's fadeIn()/fadeOut() methods
+// Could be stored in a separate utility library
+ko.bindingHandlers.fadeVisible = {
+    init: function(element, valueAccessor) {
+        // Initially set the element to be instantly visible/hidden depending on the value
+        var value = valueAccessor();
+        $(element).toggle(ko.unwrap(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
+    },
+    update: function(element, valueAccessor) {
+        // Whenever the value subsequently changes, slowly fade the element in or out
+        var value = valueAccessor();
+        ko.unwrap(value) ? $(element).fadeIn() : $(element).fadeOut();
+    }
+};
+
+$(document).ready(function () {
+    ko.applyBindings(new PlanetsModel());
+});
